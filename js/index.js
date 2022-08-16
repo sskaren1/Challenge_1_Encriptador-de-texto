2d1b71d01e66745673b51a4e9674ba9a71fb7a8b
sskaren1 <karen.93st@gmail.com>
2022-08-15 20:30:45 -0500

    add: encrypted message

---
+++ js/index.js
@@ -0,0 +1,115 @@
+const inputMessageField = document.querySelector("#inputMessage");
+const outputMessageField = document.querySelector("#outputMessage");
+const btnEncryptor = document.querySelector(".btn--encryptor");
+const btnDecryptor = document.querySelector(".btn--decryptor");
+const btnCopy = document.querySelector(".btn--copy");
+
+eventListeners();
+
+function eventListeners() {
+  inputMessageField.addEventListener("change", messageToEncrypt);
+  btnEncryptor.addEventListener("click", encryptor);
+}
+
+const objectMessage = {
+  inputMessage: "",
+  outputMessage: "",
+};
+
+const encryptionKeys = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+function messageToEncrypt(e) {
+  objectMessage[e.target.name] = e.target.value;
+  console.log("objectMessage", objectMessage);
+}
+
+function encryptor() {
+  let { inputMessage, outputMessage } = objectMessage;
+  const names = Object.keys(encryptionKeys);
+  const keys = Object.values(encryptionKeys);
+
+  if (inputMessage === "") {
+    printAlert("Debe introducir un mensaje", "error");
+  } else if (inputMessage === "A") {
+    printAlert("Debe introducir solo letras en minuscula", "error");
+  } else {
+    printAlert("Mensaje Encriptado", "success");
+
+    // Proceso de encriptación
+    for (let i = 0; i < inputMessage.length; i++) {
+      if (inputMessage[i] === names[0]) {
+        objectMessage.outputMessage += keys[0];
+
+        // otras manera de agregar o modificar un objeto
+        // objectMessage.outputMessage += keys[0];
+        // o
+        // outputMessage += keys[0];
+        // objectMessage.outputMessage = outputMessage;
+      } else if (inputMessage[i] === names[1]) {
+        objectMessage.outputMessage += keys[1];
+      } else if (inputMessage[i] === names[2]) {
+        objectMessage.outputMessage += keys[2];
+      } else if (inputMessage[i] === names[3]) {
+        objectMessage.outputMessage += keys[3];
+      } else if (inputMessage[i] === names[4]) {
+        objectMessage.outputMessage += keys[4];
+      } else {
+        objectMessage.outputMessage += inputMessage[i];
+      }
+    }
+    console.log("outputMessage final", outputMessage);
+    console.log("objectMessage final", objectMessage);
+  }
+  //   imprimir el mensaje encriptado
+  outputMessageField.innerHTML = objectMessage.outputMessage;
+  
+  //   Reiniciar el objeto
+  restartObject();
+}
+
+function isMayusc(message) {
+  for (let i = 0; i < message.lenght; i++) {}
+}
+
+function printAlert(mensaje, tipo) {
+  // Crea el div
+  const divMensaje = document.createElement("p");
+  divMensaje.classList.add("alert");
+
+  // Si es de tipo error agrega una clase
+  if (tipo === "error") {
+    divMensaje.classList.add("alert--danger");
+  } else {
+    divMensaje.classList.add("alert--success");
+  }
+
+  // Mensaje de error
+  divMensaje.textContent = mensaje;
+
+  // Insertar en el DOM
+  document
+    .querySelector("#contentEncryptor")
+    .insertBefore(divMensaje, document.querySelector(".encryptor__warning"));
+
+  // Quitar el alert despues de 3 segundos
+  setTimeout(() => {
+    divMensaje.remove();
+  }, 3000);
+}
+
+function restartObject() {
+  // Reiniciar el objeto
+  objectMessage.outputMessage = "";
+}
+
+function cleanHTML() {
+  while (outputMessageField.firstChild) {
+    outputMessageField.removeChild(outputMessageField.firstChild);
+  }
+}
